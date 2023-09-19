b8e01241bc92e912c8412dc1d0b17c2b2f7af445
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-09-19 19:16:27 -0300

    alter challenge 01 tehme JS

--- 4-JavaScript/Class-01-data-types/challenge/02-Object-game.js
+++ 4-JavaScript/Class-01-data-types/challenge/02-Object-game.js
@@ -1,13 +1,13 @@
 // Crie um programa para cadastrar Jogo que irá solicitar ao usuário o nome, o gênero e o ano de lançamento do mesmo. Em seguida, monte um OBJECT contendo essas informações e por meio dele e imprima as características do jogo usando a função log.
-let gameName = prompt("Digite seu nome do jogo:");
-let gameGenre = prompt("Digite o gênero:");
-let gameRealeaseYear = prompt("Digite o ano de lançamento:");
+const gameName = prompt("Digite seu nome do jogo:");
+const gameGenre = prompt("Digite o gênero:");
+const gameRealeaseYear = prompt("Digite o ano de lançamento:");
 
 const game = {
-    name:gameName,
-    genre:gameGenre,
-    realeaseYear:gameRealeaseYear,
-}
+  name: gameName,
+  genre: gameGenre,
+  realeaseYear: gameRealeaseYear,
+};
 
 console.log(game.name);
 console.log(game.genre);
