4b23bddb2cfa2e81183e879d7206a7f5abcc72c5
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-09-19 20:17:48 -0300

    Add Class-02-operators

---
+++ 4-JavaScript/Class-02-operators/01-arithmetic.js
@@ -0,0 +1,39 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
+// Arithmetic Operators
+
+// addition ( + )
+// subtraction ( - )
+// multiplication ( * )
+// division ( / )
+// remainder ( % )
+// exponentiation ( ** ) to square root use " ** 1/2 "
+// increment ( ++ )
+// decrement ( -- )
+
+let num1 = 2;
+let num2 = "3";
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(10 % 10);
+console.log(num1 ** num2);
+console.log(81 ** (1 / 2));
+console.log(Math.sqrt(49));
+console.log(Math.cbrt(100));
+console.log(++num1);
+console.log(--num1);
+
+// Assigment (Atribuição)
+// num1 = num1 + 5;
+// num1 += 5;
+// num1 -= 5;
+// num1 *= 5;
+// num1 /= 5;
+// num1 %= 5;
+// num1 **= 5;
+
+console.log(num1);
---
+++ 4-JavaScript/Class-02-operators/02-comparation.js
@@ -0,0 +1,19 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
+// Comparison Operators (Boolean)
+// >    >=
+// <    <=
+// ==   !=
+// ===  !==
+
+
+console.log(10 > 10);
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 != 20);
+console.log(10 >= 20);
+console.log(10 <= 20);
+console.log(10 === 10); //Value and type
+console.log(10 === "10");
+console.log(10 !== 10);
+console.log(10 !== "10");
