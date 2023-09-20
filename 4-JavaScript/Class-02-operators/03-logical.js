39e212e5fceb5859ca1513288c4d98e0132854d8
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-09-20 20:26:54 -0300

    add class-02 operators-logicals

---
+++ 4-JavaScript/Class-02-operators/03-logical.js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
+// Logical Operators (Boolean)
+
+// and  && "E"
+// or   || "OU"
+// not  !  "Não"
+
+//Go to Walt Disney?
+
+const creditCard = true;
+const hasCredit = false;
+
+console.log(creditCard);
+console.log(creditCard && hasCredit);
+console.log(!creditCard);
+
+const saveMoney = true;
+const getLoan = true;
+
+console.log(saveMoney || getLoan);
+console.log(!saveMoney || !getLoan);
