a79ca056af696bb19211ed06e38d22b9a7399969
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-09-21 23:00:37 -0300

    add challenge resolution in the Class 02 operators

--- 4-JavaScript/Class-02-operators/challenge/evenOdd.js
+++ 4-JavaScript/Class-02-operators/challenge/evenOdd.js
@@ -1,3 +1,8 @@
 // Construa uma lógica para dizer se um número é par ou é ímpar
 // Receba o número desejado do usuário via promp
 // Escreve na tela o número e se ele é par ou é ímpar
+
+const num1 = Number(prompt("Digite um número para saber se é par ou é ímpar:"));
+const result = num1 % 2 == 0 ? "Par" : "Impar";
+
+window.alert(`O número é ${result}`);
