c445308bcf636ef5b9cf09584e992a0742e86dd9
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-09-22 23:11:47 -0300

    challenge resolution in the class 03 functions

--- 4-JavaScript/Class-03-functions/challenge/convertMsToKmh.js
+++ 4-JavaScript/Class-03-functions/challenge/convertMsToKmh.js
@@ -1,3 +1,7 @@
 // Crie uma função para converter velocidade de m/s para km/h
 // Solicite a velocidade ao usuário via prompt
 // Escreva (write) na tela o resultado
+
+const userSpeed = Number(prompt("Digite a veocidade em m/s:"));
+const convertMsToKmh = (speed) => speed * 3.6;
+document.write(`${userSpeed}m/s é igual a ${convertMsToKmh(userSpeed)}km/h`);
