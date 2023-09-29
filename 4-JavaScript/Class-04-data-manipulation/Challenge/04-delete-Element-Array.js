a82e85ec3a725c7f3d1326e37ba6325f6befd3dd
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-09-29 19:34:37 -0300

    alter file in the class-04 data manipulation

--- 4-JavaScript/Class-04-data-manipulation/Challenge/04-delete-Element-Array.js
+++ 4-JavaScript/Class-04-data-manipulation/Challenge/04-delete-Element-Array.js
@@ -2,11 +2,13 @@
 // Insira o nome do elemento e o array via parâmetros
 // Retorne o array com valor atualizado
 
-function deleteElementArray(element, array) {
+const techs = ["HTML", 10, "CSS", 20, "JavaScript", 30];
+
+function deleteArrayElement(element, array) {
   const index = array.indexOf(element);
   index != -1 ? array.splice(index, 1) : "";
   return array;
 }
 
-const techs = ["'HTML", 10, "CSS", 20, "JavaScript", 30];
-console.log(deleteElementArray("HTML", techs));
+const newArray = deleteArrayElement("JavaScript", techs);
+document.write(newArray);
