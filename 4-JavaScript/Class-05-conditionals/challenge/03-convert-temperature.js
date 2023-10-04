6b005f98323d090141d295d3998b15de53bdf8ac
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-10-04 12:30:25 -0300

    alter file challenge convert temperature

--- 4-JavaScript/Class-05-conditionals/challenge/03-convert-temperature.js
+++ 4-JavaScript/Class-05-conditionals/challenge/03-convert-temperature.js
@@ -10,45 +10,35 @@ function convertTemperature(temp) {
   let degreeArray = Array.from(degreeTemp);
   let indexF = degreeArray.indexOf("F");
   let indexC = degreeArray.indexOf("C");
+  let scaleDegree;
+  let newDegree;
 
-  if (indexF == -1 && indexC == -1) {
-    throw new Error(
-      "Escala não identificada, insira 'C' ou 'F': Ex: 10F ou 20C."
-    );
-  } else if (indexF != -1 && indexC != -1) {
-    throw new Error(
-      "Indique apenas uma escala por vez, insira 'C' ou 'F': Ex: 10F ou 20C."
-    );
+  if (indexF != -1) {
+    scaleDegree = degreeArray.splice(indexF, 1).join("");
+  } else if (indexC != -1) {
+    scaleDegree = degreeArray.splice(indexC, 1).join("");
+  }
+
+  newDegree = Number(degreeArray.join(""));
+
+  if (isNaN(newDegree) || (scaleDegree !== "C" && scaleDegree !== "F")) {
+    throw new Error("Valor inválido! Insira conforme exemplo: 10F ou 20C.");
+  } else if (scaleDegree == "C") {
+    return ((newDegree * 9) / 5 + 32).toLocaleString("pt-BR") + "F";
+  } else if (scaleDegree == "F") {
+    return (((newDegree - 32) * 5) / 9).toLocaleString("pt-BR") + "C";
   } else {
-    let scaleDegree;
-
-    if (indexF != -1) {
-      scaleDegree = degreeArray.splice(indexF);
-    } else if (indexC != -1) {
-      scaleDegree = degreeArray.splice(indexC);
-    }
-
-    let newDegree = Number(degreeArray.join(""));
-
-    if (isNaN(newDegree)) {
-      throw new Error("Valor inválido! Insira conforme exemplo: 10F ou 20C.");
-    } else {
-      if (scaleDegree == "C") {
-        return ((newDegree * 9) / 5 + 32).toLocaleString("pt-BR") + "F";
-      } else if (scaleDegree == "F") {
-        return (((newDegree - 32) * 5) / 9).toLocaleString("pt-BR") + "C";
-      } else {
-        throw new Error(
-          "Escala não identificada, insira 'C' ou 'F': Ex: 10F ou 20C."
-        );
-      }
-    }
+    throw new Error("Escala não identificada, insira 'C' ou 'F': Ex: 10F ou 20C.");
   }
 }
 
 try {
-  let temperature = " 20    F";
-  console.log(`${temperature.toUpperCase().split(" ").join("")} = ${convertTemperature(temperature)}`);
+  let temperature = "2c0";
+  console.log(
+    `${temperature.toUpperCase().split(" ").join("")} = ${convertTemperature(
+      temperature
+    )}`
+  );
 } catch (error) {
   console.log(error);
 }
