face51d295cd58a23f0730d389c6235dd12e02c7
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-10-10 20:43:21 -0300

    add correction challenge 05 divinate game in the theme js

---
+++ 4-JavaScript/Class-06-loops/challenge/03-convertToBinary.js
@@ -0,0 +1,26 @@
+// Crie um algoritmo que receba um número qualquer
+// e apresente para o usuario o numero binario desse velor
+function decimalToBinary(number) {
+  let result = [];
+  if (number == null) {
+    throw Error("Numero não identificado!");
+  } else if (number < 0) {
+    throw Error("Não permitimos numeros Menor que 0!");
+  }
+}
+console.log(decimalToBinary(-1)); // Deve imprimir '1010'
+console.log(decimalToBinary(27)); // Deve imprimir '11011'
+console.log(decimalToBinary(100)); // Deve imprimir '1100100'
+
+0 = 42/2
+1 = 21/2
+0 = 10/2
+1 = 5/2
+0 = 2/2
+1 = 1/2
+
+1 = 27/2
+1 = 13/2
+0 = 6/2
+1 = 3/2
+1 = 1/2
--- 4-JavaScript/Class-06-loops/challenge/05-dvination-game.js
+++ 4-JavaScript/Class-06-loops/challenge/05-dvination-game.js
@@ -1,33 +1,29 @@
-// Crie um algoritmo que adivinhe um número de 1 a 100
+// Crie um algoritmo que adivinhe um número de 1 a 10
 // Capture o número de tentativas enquando não acertar
 // O programa deve ser encerrado caso o usuário clique em cancelar
 
-let numberDivination = Math.ceil(Math.random() * 100);
-
+const numberDivination = Math.ceil(Math.random() * 10);
 let count = 1;
+
 while (true) {
-  let numberUser = prompt("Advinhe um número de 1 a 100:");
+  let numberUser = prompt("Advinhe um número de 1 a 10:");
 
   if (numberUser == null) {
     alert("Programa encerrado!");
     break;
-  } else if (numberUser == "" || numberUser == " ") {
-    alert("Insira um número de  1 a 100!");
+
+  }else if (numberUser == "" || isNaN(numberUser)) {
+    alert("Insira um número de  1 a 10!");
+    continue;
+
   } else if (numberUser > 100 || numberUser < 1) {
-    alert("Atenção, número fora do intervalo de 1 a 100");
-  } else {
-    if (Number(numberUser) > numberDivination) {
-      alert("Menos");
-    }
-    if (Number(numberUser) < numberDivination) {
-      alert("Mais");
-    }
-    if (Number(numberUser) == numberDivination) {
-      alert(
-        `Parabêns! Acertou o número ${numberDivination} em ${count} tentativa(s)`
-      );
-      break;
-    }
+    alert("Atenção, número fora do intervalo de 1 a 10");
+    continue;
+
+  }else if (Number(numberUser) == numberDivination) {
+    alert(`Parabêns! Acertou o número ${numberDivination} em ${count} tentativa(s)`);
+    break;
   }
+  
   count++;
 }
