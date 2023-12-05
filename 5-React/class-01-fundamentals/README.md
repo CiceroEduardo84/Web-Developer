90230f373d4d5adfb68c6602266c6ac5d77fcf09
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-12-05 20:24:31 -0300

    add class 01 fundamentals exemples

---
+++ 5-React/class-01-fundamentals/README.md
@@ -0,0 +1,8 @@
+# Class 01 Fundamentals
+
+## Topics
+
+- Introduction React Library
+- SPA & SSR
+- Installantion
+- Syntex
---
+++ 5-React/class-01-fundamentals/src/App.tsx
@@ -0,0 +1,14 @@
+import imageReact from "./assets/react.svg";
+
+// Named
+export function App() {
+  // Fragment
+  return (
+    <>
+      <h1> Class 01 Fundamentals</h1>
+      <p>Syntaxe JSX = JS + XML/HTML</p>
+      <img src="vite.svg" alt="" />
+      <img src={imageReact} alt="" />
+    </>
+  );
+}
