dfd0b49e1508d7147c249002605011aa4b1943cf
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-12-22 23:34:51 -0300

    add reviewing react fundamentals

---
+++ Estudos/Reviewing_React/01-fundamentals/src/App.tsx
@@ -0,0 +1,11 @@
+// Named = Pascal in the name functions
+export function App() {
+  return (
+    // Fragment
+    <>
+      <h1>Reviewing React Fundamentals</h1>
+      <p>ok</p>
+      <img src="vite.svg" alt="" />
+    </>
+  );
+}
