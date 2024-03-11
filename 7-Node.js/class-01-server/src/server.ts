796ba93e37c511d2bb17f97256a50e0f8e71bae6
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-03-11 20:43:08 -0300

    add class 01 node server

---
+++ 7-Node.js/class-01-server/src/server.ts
@@ -0,0 +1,12 @@
+import express, { request, response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT: ${port}`);
+});
+
+app.route("/").get((_request, response) => {
+  response.send({ className: "Class 01 Server" });
+});
