cf773a72ef790c259df8fd47fc73489ac5c0fe08
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-03-14 20:53:34 -0300

    add class 03 controllers

---
+++ 7-Node.js/class-03-controllers/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { router } from "./routes";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT: http://localhost:${port}/`);
+});
+
+//middleware
+app.use(express.json());
+app.use(router);
