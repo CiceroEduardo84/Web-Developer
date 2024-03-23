328b20dd5b0c722c2351345e22b136a9559003e8
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-03-22 21:10:15 -0300

    add create uuid and password hash and read controller

--- 7-Node.js/class-04-databases/src/controllers/index.ts
+++ 7-Node.js/class-04-databases/src/controllers/index.ts
@@ -1,24 +1,53 @@
 import { Request, Response, NextFunction } from "express";
 import { sqliteConnection } from "../databases/sqlite3";
+import { randomUUID } from "node:crypto";
+import { hash, compare } from "bcrypt";
 
 export const userControllers = {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const { id, name, email, password } = req.body;
+      const { name, email, password } = req.body;
+      const uuid = randomUUID();
       const db = await sqliteConnection();
+      const passwordHash = await hash(password, 10);
       await db.run(
         "INSERT INTO users (Id, name, email, password) VALUES (?,?,?,?);",
-        [id, name, email, password]
+        [uuid, name, email, passwordHash]
       );
 
-      return res.json({ message: "created!", id });
+      return res.status(201).json({ message: "created!", id: uuid });
     } catch (error) {
       next(error);
     }
   },
 
-  async read(req: Request, res: Response,) {
-    res.send({ message: "read!" });
+  async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { password } = req.body;
+
+      if (!password) {
+        throw res
+          .status(400)
+          .json({ message: "Please, confirm your password!" });
+      }
+
+      const db = await sqliteConnection();
+      const user = await db.get("SELECT * FROM users WHERE id = ?;", [id]);
+
+      if (!user) throw res.status(404).json({ message: "User not found!" });
+
+      const passwordCheck = await compare(password, user.password);
+      if (!passwordCheck) {
+        throw res.status(400).json({ message: "Invalid password!" });
+      }
+
+      const { name, email } = user;
+
+      return res.json({ name, email });
+    } catch (error) {
+      next(error);
+    }
   },
 
   async update(req: Request, res: Response) {
